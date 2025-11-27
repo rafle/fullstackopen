f2e8a7e3c1a1158f65d37892ef703fd798488f1f
Rafael Leal <rafael.leal@aalto.fi>
2025-11-27 13:21:02 +0200

    1.11 Milestone

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,25 +1,41 @@
 import { useState } from 'react'
 
-const Display = () => <div>give feedback</div>
+const Display = () => <h2>give feedback</h2>
 
 const Button = (props) => (
   <button onClick={props.onClick}>{props.text}</button>
 )
+
+const StatisticLine = ({text, value}) => { 
+  return (
+    <tbody>
+      <tr>
+        <td>{text}</td> 
+        <td>{value}</td>
+      </tr>
+    </tbody>
+  )
+}
 
 const Statistics = ({good, bad, neutral}) => {
   const total = good + neutral + bad ;
   if (!total) {
     return <p>No feedback given</p>
   }
+  // if fact these ternaries are already handled above
+  const average = total ? (good - bad) / total : 0
+  const positive = total ? (good / total) * 100 : 0
   return (
     <div>
-      <p>statistics</p>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {total ? (good - bad) / total : 0 }</div>
-      <div>positive {total ? (good / total) * 100 : 0} %</div>
+      <h2>statistics</h2>
+      <table>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={total} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive + "%"} />
+      </table>
     </div>
   )
 }
@@ -37,7 +53,6 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text='good' />
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button onClick={() => setBad(bad + 1)} text='bad' />
-      {/* code here */}
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
